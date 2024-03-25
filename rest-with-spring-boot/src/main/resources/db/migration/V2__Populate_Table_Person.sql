4eb15cac91520b0b5793399f6f0b094c75bf4448
marcoantoniosilvapegado <marcoantoniosilvapegado@gmail.com>
2024-03-24 22:57:30 -0300

    delel

--- rest-with-spring-boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-	(1, 'rua 33', 'marcao', 'home', 'antoniao'),
-	(2, 'India', 'gandhi', 'home?', 'Mahatma'),
-	(4, 'Anchiano - italy', 'leonardo', 'home', 'da vinci'),
-	(6, 'Anchiano - italy', 'jhon', 'home', 'travolta'),
-	(7, 'Africa', 'Fredie', 'home', 'Mercury');
